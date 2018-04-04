3c39b13dfefcaebb669def4d92505b1e6a9433c0
Eugene Yeo <yeo.eugene.oey@gmail.com>
2018-04-05 01:10:56 +0800

    addition of mask layer

--- __init__.py
+++ __init__.py
@@ -107,12 +107,18 @@ class PoolVision(object):
     def detectCueBall(self):
         # Find White Circle
         white_circle = cv2.inRange(self.hsv, np.array([0, 0, 110]), np.array([180, 30, 255]))
-        whiteball = cv2.bitwise_and(white_circle, white_circle, mask=self.table_mask)
-        whiteball = cv2.dilate(whiteball, np.ones((5,5),np.uint8), iterations=2)
-        whiteball = cv2.erode(whiteball, np.ones((5,5),np.uint8), iterations=4)
-        whiteball = cv2.dilate(whiteball, np.ones((5,5),np.uint8), iterations=3)
+        whiteball_mask = cv2.bitwise_and(white_circle, white_circle, mask=self.table_mask)
+        whiteball_mask = cv2.dilate(whiteball_mask, np.ones((5,5),np.uint8), iterations=2)
+        whiteball_mask = cv2.erode(whiteball_mask, np.ones((5,5),np.uint8), iterations=4)
+        whiteball_mask = cv2.dilate(whiteball_mask, np.ones((5,5),np.uint8), iterations=3)
+
+        ctrs, hierarchy = cv2.findContours(whiteball_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1:]
+        cv2.drawContours(self.raw_frame, ctrs, -1, (0,255,0), 2)
+
+
+
         
-        self.cueball_features = cv2.goodFeaturesToTrack(self.gray, maxCorners=5, qualityLevel=0.01, minDistance=5, mask=whiteball)
+        self.cueball_features = cv2.goodFeaturesToTrack(self.gray, maxCorners=5, qualityLevel=0.01, minDistance=5, mask=whiteball_mask)
         if self.cueball_features is not None:
             for f in self.cueball_features:
                 x,y = f.ravel()
@@ -128,15 +134,40 @@ class PoolVision(object):
 
     def detectBoundary(self):
         table_edge = cv2.bitwise_and(self.edge, self.edge, mask=self.table_mask)
-        lines = cv2.HoughLinesP(table_edge, rho=1, theta=np.pi/180, threshold=10, minLineLength=130, maxLineGap=10)
+        #lines = cv2.HoughLinesP(table_edge, rho=1, theta=np.pi/180, threshold=10, minLineLength=130, maxLineGap=10)
 
         # TODO: Need to determine the table region from the detected lines.
-        return lines
+        return []
 
 
     def detectCircle(self):
-        self.cloth = cv2.bitwise_and(self.gray, self.gray, mask=self.table_mask)
-        return cv2.HoughCircles(self.cloth, method=cv2.HOUGH_GRADIENT, dp=1, minDist=12, param1=20, param2=7, minRadius=14, maxRadius=15)[0]
+        pocket_color = cv2.inRange(self.hsv, np.array([0, 0, 0]), np.array([180, 255, 50]))
+        table_mask = cv2.bitwise_and(self.table_mask, cv2.bitwise_not(pocket_color))
+        table_mask = cv2.dilate(table_mask, np.ones((3,3), np.uint8), iterations=3)
+
+        # Exclude All having blue cloth colors
+        blue_cloth_mask = cv2.inRange(self.hsv, np.array([100, 10, 10]), np.array([110, 200, 255]))
+        ball_mask = cv2.bitwise_not(blue_cloth_mask, mask=table_mask)
+
+        # Exclude Pockets
+        #pocket_color = cv2.inRange(self.hsv, np.array([0, 0, 200]), np.array([255, 255, 255]))
+
+        # Remove Noise (white patches on the table cloth)
+        ball_mask = cv2.erode(ball_mask, np.ones((5,5), np.uint8), iterations=3)
+        ball_mask = cv2.dilate(ball_mask, np.ones((5,5), np.uint8), iterations=4)
+
+        #cv2.imshow('xx', cv2.inRange(self.hsv, np.array([100, 0, 0]), np.array([105, 255, 255])))
+        ball_candidate = cv2.bitwise_and(self.gray, self.gray, mask=ball_mask)
+        cv2.imshow('xx', ball_candidate)
+
+
+
+        #ctrs, hierarchy = cv2.findContours(table, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1:]
+        #cv2.drawContours(self.raw_frame, ctrs, -1, (255,255,0), 2)
+        # self.ball_mask = cv2.inRange(self.hsv, np.array([100, 60, 60]), np.array([110, 255, 255]))
+        # self.ball_mask = cv2.bitwise_and(ball_mask, ball_mask, mask=self.table_mask)
+        # self.cloth = cv2.bitwise_and(self.gray, self.gray, mask=self.ball_mask)
+        return cv2.HoughCircles(ball_candidate, method=cv2.HOUGH_GRADIENT, dp=1, minDist=14, param1=300, param2=4, minRadius=14, maxRadius=15)[0][:10]
 
     def overlayCircle(self, circle):
         cv2.circle(self.raw_frame, (circle[0], circle[1]), circle[2], (0, 255,0))
@@ -164,7 +195,7 @@ class PoolVision(object):
 
         # TODO: Ball Detection
         self.detectCueBall()
-        self.detectObjectBalls()
+        #self.detectObjectBalls()
 
         # TODO: Ball tracking
         self.trackCueBall()
