b9215a932ff8ca8c1e05c345ba36ff3a2a261aba
Eugene Yeo <yeo.eugene.oey@gmail.com>
2018-04-05 09:14:29 +0800

    polishing code

--- __init__.py
+++ __init__.py
@@ -136,7 +136,9 @@ class PoolVision(object):
         cueball_track = iter(self.cueball_track)
         prev_cueball = next(cueball_track)
         for curr_cueball in cueball_track:
-            cv2.line(self.raw_frame, prev_cueball, curr_cueball, (255,0,0), 2)
+            x = np.linalg.norm(np.array(curr_cueball)-np.array(prev_cueball))
+            if x < 50:
+                cv2.line(self.raw_frame, prev_cueball, curr_cueball, (255,0,0), 2)
             prev_cueball = curr_cueball
 
     def detectObjectBalls(self):
@@ -156,16 +158,18 @@ class PoolVision(object):
 
 
     def detectCircle(self):
-        pocket_color = cv2.inRange(self.hsv, np.array([0, 0, 0]), np.array([180, 255, 50]))
-        table_mask = cv2.bitwise_and(self.table_mask, cv2.bitwise_not(pocket_color))
-        table_mask = cv2.dilate(table_mask, np.ones((3,3), np.uint8), iterations=3)
+        blue_cloth_mask = cv2.inRange(self.hsv, np.array([100, 10, 10]), np.array([110, 200, 255]))
+        blue_cloth_mask = cv2.dilate(blue_cloth_mask, np.ones((5,5), np.uint8), iterations=3)
+        blue_cloth_mask = cv2.erode(blue_cloth_mask, np.ones((5,5), np.uint8), iterations=4)
+        table_mask = cv2.bitwise_and(self.table_mask, blue_cloth_mask)
+        ctrs, hier = cv2.findContours(table_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1:]
+        cv2.drawContours(table_mask, ctrs, 0, 255, -1)
+        cv2.imshow('a', table_mask)
+        
 
         # Exclude All having blue cloth colors
-        blue_cloth_mask = cv2.inRange(self.hsv, np.array([100, 10, 10]), np.array([110, 200, 255]))
         ball_mask = cv2.bitwise_not(blue_cloth_mask, mask=table_mask)
-
-        # Exclude Pockets
-        #pocket_color = cv2.inRange(self.hsv, np.array([0, 0, 200]), np.array([255, 255, 255]))
+        cv2.imshow('b', blue_cloth_mask)
 
         # Remove Noise (white patches on the table cloth)
         ball_mask = cv2.erode(ball_mask, np.ones((5,5), np.uint8), iterations=3)
@@ -182,9 +186,10 @@ class PoolVision(object):
         # self.ball_mask = cv2.inRange(self.hsv, np.array([100, 60, 60]), np.array([110, 255, 255]))
         # self.ball_mask = cv2.bitwise_and(ball_mask, ball_mask, mask=self.table_mask)
         # self.cloth = cv2.bitwise_and(self.gray, self.gray, mask=self.ball_mask)
-        circles = cv2.HoughCircles(ball_candidate, method=cv2.HOUGH_GRADIENT, dp=1, minDist=14, param1=300, param2=4, minRadius=14, maxRadius=15)[0][:10]
-        for circle in circles:
-            self.overlayCircle(circle)
+        circles = cv2.HoughCircles(ball_candidate, method=cv2.HOUGH_GRADIENT, dp=1, minDist=14, param1=300, param2=4, minRadius=14, maxRadius=15)
+        if circles is not None:
+            for circle in circles[0][:10]:
+                self.overlayCircle(circle)
 
     def overlayCircle(self, circle):
         cv2.circle(self.raw_frame, (circle[0], circle[1]), circle[2], (0, 255,0))
